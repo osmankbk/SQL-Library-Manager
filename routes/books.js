a90fc66ab1e62e7a2142ffe035f1ea833ee24525
osmankbk <32382267+osmankbk@users.noreply.github.com>
2020-04-02 20:25:39 -0400

    connect DB to update and delete pug

--- routes/books.js
+++ routes/books.js
@@ -33,22 +33,22 @@ router.get('/books/new', asyncBubble(async(req, res) => {
 router.post('/', asyncBubble(async(req, res) => {
     const book = await Book.create(req.body);
     console.log(req.body);
-    res.redirect("/books" + book.id, {book, title: book.title});
+    res.redirect("/books", {book, title: book.title});
 }));
 
 //Individual book by id
 router.get("/books/:id", asyncBubble(async(req, res) => {
     const book = await Book.findByPk(req.params.id);
-    res.render('update-book', {});
+    res.render('update-book', {book});
 }));
 
 //Update book
-router.get("/books/:id/update", asyncBubble(async(req, res) => {
+router.get("/books/:id", asyncBubble(async(req, res) => {
     const book = await Book.findByPk(req.params.id);
-    res.render('update-book', {book: book});
+    res.render('update-book', {book});
 }));
 
-router.post("/books/:id/update", asyncBubble(async(req, res) => {
+router.post("/books/:id", asyncBubble(async(req, res) => {
     const book = await Book.findByPk(req.params.id);
     await book.update(req.body);
     res.render("/books/" + book.id);
@@ -56,15 +56,15 @@ router.post("/books/:id/update", asyncBubble(async(req, res) => {
 
 //DELETE
 
-router.get("/books/:id/delete", asyncBubble(async(req, res) => {
+router.get("/books/:id", asyncBubble(async(req, res) => {
     const book = await Book.findByPk(req.params.id);
     res.render('update-book', {book: book});
 }));
  
-router.post("/books/:id/delete", asyncBubble(async(req, res) => {
+router.post("/books/:id", asyncBubble(async(req, res) => {
     const book = await Book.findByPk(req.params.id);
     await book.destroy();
-    res.redirect('/books/index');
+    res.redirect('/books/');
 }));
 
 module.exports = router;
